67107b64539e3c288dae83a9839203c5a597b153
robinmahto <robinmahto413@gmail.com>
2024-03-31 17:22:54 +0530

    expeicence section modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,12 @@ import { styles } from "../global";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { fadeIn, textVariant } from "../utils/motion";
 import { about_avatar } from "../assets";
- 
+
 const About = () => {
   return (
     <div className="flex flex-col md:flex-row items-center justify-between gap-6">
       <div>
-         <img src={about_avatar} alt="about_avatar" className="w-96 h-96" />
+        <img src={about_avatar} alt="about_avatar" className="w-96 h-96" />
       </div>
       <div>
         <motion.div variants={textVariant()}>
@@ -22,11 +22,10 @@ const About = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          I'm a skilled software developer with experience in TypeScript and
-          JavaScript, and expertise in frameworks like React.js, Redux.js, and
-          Node.js. I'm a quick learner and collaborate closely with clients to
-          create efficient, scalable, and user-friendly solutions that solve
-          real-world problems. Let's work together to bring your ideas to life!
+          Experienced Frontend Web Developer with over 2 years of hands-on
+          experience in JavaScript, TypeScript, and React.js. committed to
+          staying updated with the latest technologies and best practices in the
+          industry.
         </motion.p>
       </div>
     </div>
